bbc679116d6c0e7181c9701d972bb648750ef56f
Lorena Barreto <lorenasimedo@gmail.com>
2024-05-22 08:53:51 -0400

    Updates and running the code for all csv bugs

--- llm-bug-localization.py
+++ llm-bug-localization.py
@@ -217,9 +217,14 @@ def get_method_body_by_method_signature(method_signature: str) -> str:
         # Finally, separate the package name from the class name by splitting at the last dot in package_class
         package_name, class_name = package_class.rsplit('.', 1)
     elif "$" in method_signature:
-        print("6!")
-        package_class, member_name = method_signature.rsplit('.', 1)
-        package_name, class_name = package_class.rsplit('.', 1)
+        if project == "Csv":
+            print("5.5!")
+            package_class, member_name = method_signature.rsplit('.', 1)
+            package_name, class_name = package_class.rsplit('$', 1)
+        else:
+            print("6!")
+            package_class, member_name = method_signature.rsplit('.', 1)
+            package_name, class_name = package_class.rsplit('.', 1)
     else:
         print("7!")
         parts = method_signature.split('.')
